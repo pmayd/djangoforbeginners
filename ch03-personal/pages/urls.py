833f3163215e45fe63ef91f739111cc89ac6f233
Michael Aydinbas <michael.aydinbas@gmail.com>
2025-03-10 00:23:19 +0100

    reorder urls

--- ch03-personal/pages/urls.py
+++ ch03-personal/pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import home_page_view, about_page_view  # new
 
 urlpatterns = [
-    path("about/", about_page_view),  # new
     path("", home_page_view),
+    path("about/", about_page_view),  # new
 ]
